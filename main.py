3cb2c872f1c5af451a43b6fda943e5efa22c4b8e
John Osborn <jposborn@outlook.com>
2025-12-05 11:49:12 +1100

    Update LLM to gpt-5-nano

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ Musk's political activities, views, and statements have made him a polarizing fi
     )
 
     # llm = ChatOllama(temperature=0, model="gemma3:270m")
-    llm = ChatOpenAI(temperature=0, model="gpt-5")
+    llm = ChatOpenAI(temperature=0, model="gpt-5-nano")
     chain = summary_prompt_template | llm
 
     response = chain.invoke(input={"information": information})
